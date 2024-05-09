91c90142e135f803fcb3ead8c4ad9332fd73fe64
Jewel Chowdhury <103192937+DarkXAce01@users.noreply.github.com>
2024-05-09 18:41:30 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (52.13%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long int num=0;
+        while(x!=0){
+        num=num*10 + x%10;
+        x=x/10;
+        }
+        if(num<INT_MIN || num>INT_MAX){
+        return(0);
+        }
+        else{
+            return(num);
+        }
+    }
+};
